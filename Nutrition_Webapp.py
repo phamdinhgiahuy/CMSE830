087680472542b611570be39ff0794577ae38f72a
Gia Huy <giahuyab1@gmail.com>
2023-10-14 18:22:02 -0400

    second step done

--- Nutrition_Webapp.py
+++ Nutrition_Webapp.py
@@ -1,30 +1,62 @@
-import pandas as pd
+#Packages
 import streamlit as st
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import plotly.express as px
 import time
 
-#path = r'C:\Users\Admin\OneDrive - Michigan State University\Courses\CMSE 830\HW and Assignment\Dataset\Food_nutrition.csv'
+
+#Load dataset
+nutri_ori = pd.read_csv('Food_nutrition.csv').dropna()
+nutri = nutri_ori[:1000]
 
 
-col1, col2, col3 = st.columns([1, 2, 1])
+macronutrients = ['Water_(g)', 'Protein_(g)', 'Lipid_Tot_(g)', 'Carbohydrt_(g)', 'Fiber_TD_(g)']
+minerals = [
+    'Calcium_(mg)', 'Iron_(mg)',
+       'Magnesium_(mg)', 'Phosphorus_(mg)', 'Potassium_(mg)', 'Sodium_(mg)',
+       'Zinc_(mg)', 'Copper_mg)', 'Manganese_(mg)', 'Selenium_(µg)',
+        'Thiamin_(mg)', 'Riboflavin_(mg)', 'Niacin_(mg)',
+       'Panto_Acid_mg)',  'Folate_Tot_(µg)', 'Folic_Acid_(µg)',
+       'Food_Folate_(µg)', 'Folate_DFE_(µg)', 'Choline_Tot_ (mg)',
+        'Retinol_(µg)',
+       'Alpha_Carot_(µg)', 'Beta_Carot_(µg)', 'Beta_Crypt_(µg)',
+       'Lycopene_(µg)', 'Lut+Zea_ (µg)', 
+        'FA_Sat_(g)', 'FA_Mono_(g)', 'FA_Poly_(g)',
+]
+vitamins = [col for col in nutri.columns if col.startswith('Vit_')]
 
-col1.markdown(" ## Navigation: ")
-col1.markdown(" ### Start Here! ")
+#Function
 
-file = col2.file_uploader("Upload your dataset here!")
-if file is None:
-    col2.markdown(" No files uploaded yet. ")
-else:
-    bar = col2.progress(0)
-    for per in range(100):
-        time.sleep(0.01)
-        bar.progress(per+1)
+#User information
+with st.expander("Click here to view your dataset infomation"):
+    st.write("Data sample")
+    st.write(nutri.head())
+    st.write("Data summary")
+    st.write(nutri.describe())
 
-    col2.success("Your dataset has been uploaded!")
-    nutri = pd.read_csv(file)
-    col3.metric(label="Data Rows Count", value = f'{nutri.shape[0]}')
-    with st.expander("Click here to view your dataset infomation"):
-        st.write("Data sample")
-        st.write(nutri.head())
-        st.write("Data summary")
-        st.write(nutri.describe())
-            
+# Dataset selector
+#dataset_option = st.selectbox('Select dataset:', ['Sample Dataset', 'Seaborn MPG'])
+
+
+#y_var = st.selectbox('Select y-variable:', [col for col in nutri.columns if nutri[col].dtype != 'object'])
+#selected_df = nutri[[x_var, y_var]].dropna()
+
+#Visualization 
+st.subheader("Nutrition Quick View")
+tab1, tab2, tab3 = st.tabs(["Macronutrients, Calories and Cholesterol", "Essential Minerals", "Vital Vitamins"])
+with tab1:
+    x1_var = st.selectbox('Choose your Macronutrients:', macronutrients)
+    y1_var = st.selectbox('Weight Control or Heart Heath Promotion?', ['Energ_Kcal', 'Cholestrl_(mg)'])
+    fig1 = px.scatter(nutri, x=x1_var, y=y1_var, hover_data='Shrt_Desc', marginal_x="histogram", marginal_y="histogram")
+    st.plotly_chart(fig1, theme="streamlit", use_container_width=True)
+with tab2:
+    options2 = st.multiselect("Which Essential Minerals do you care about?", minerals)
+    #st.pyplot(fig2, use_container_width=True)
+with tab3:
+    options3 = st.multiselect("Which Vital Vitamins do you care about?", vitamins)
+    #st.pyplot(fig3, use_container_width=True)
+
+
